b1ecfe42642a3e211669c454cf6c224aed65df93
uma <developer333u@gmail.com>
2025-02-10 12:33:56 +0300

    update

--- src/main/java/com/gridnine/testing/service/FlightService.java
+++ src/main/java/com/gridnine/testing/service/FlightService.java
@@ -7,5 +7,5 @@ import java.util.List;
 public interface FlightService {
     List<Flight> departureBeforeThatTime(List<Flight> flights);
     List<Flight> arrivalDateBeforeDepartureDate(List<Flight> flights);
-    public List<Flight> timeOnEarth(List<Flight> flights);
+    List<Flight> timeOnEarth(List<Flight> flights);
 }
